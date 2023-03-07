ec0e7fae3e53aa85cc0adeb77adee323abf6e0be
b-sandrine <sanrdushimimana@gmail.com>
2023-03-07 09:19:37 +0200

    Added ejs settings

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var express = require("express")
 var cors = require("cors")
 var connect = require('./src/config/connection')
 const { json } = require('express')
+const path = require('path')
+
 
 var app = express();
+app.set('views', path.join(__dirname, '/src/views'))
+app.set('view engine', 'ejs')
 
 // requiring dotenv & loading env variables 
 require('dotenv').config()
@@ -17,15 +21,13 @@ app.use(cors())
 app.use(json())
 
 app.get("/",(req,res) => {
-    res.send("Welcome to backend")
+    // res.send("Welcome to backend")
+    res.render('index')
 })
 
 app.use('/api/users',router);
 
-
-
 const PORT = process.env.PORT || 7000
-
 
 app.listen(PORT, () => {
     console.log("http://localhost:"+PORT)
